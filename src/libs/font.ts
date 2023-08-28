30d91149511ec3805e821eafab886168064f0f97
C2Coder <102322220+C2Coder@users.noreply.github.com>
2023-08-29 01:24:08 +0200

    Update spacing

--- src/libs/font.ts
+++ src/libs/font.ts
@@ -1,4 +1,4 @@
-export const spacing = 1;
+export const spacing = 2;
 
 const symbols = [" ", "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"]
 const letters = [
